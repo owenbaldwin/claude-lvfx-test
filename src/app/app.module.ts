59e97df9806353a6d75d79270a07f5ebacfaea4c
Owen Baldwin <90186455+owenbaldwin@users.noreply.github.com>
2025-02-25 19:52:13 +0000

    Add Angular app module and core components

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductionBreakdownComponent } from './components/production-breakdown/production-breakdown.component';
+import { BreakdownRowComponent } from './components/breakdown-row/breakdown-row.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/breakdown', pathMatch: 'full' },
+  { path: 'breakdown', component: ProductionBreakdownComponent },
+  { path: '**', redirectTo: '/breakdown' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProductionBreakdownComponent,
+    BreakdownRowComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
