e1a2f42f905ca94faf0eb3774d1633511c61c92a
Owen Baldwin <90186455+owenbaldwin@users.noreply.github.com>
2025-02-25 19:56:20 +0000

    Update production breakdown component to use service

---
+++ src/app/components/production-breakdown/production-breakdown.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Production } from '../../models/production.model';
+import { ProductionService } from '../../services/production.service';
+
+@Component({
+  selector: 'app-production-breakdown',
+  templateUrl: './production-breakdown.component.html',
+  styleUrls: ['./production-breakdown.component.scss']
+})
+export class ProductionBreakdownComponent implements OnInit {
+  production: Production;
+  selectedTab: string = 'Breakdown';
+  searchTerm: string = '';
+  
+  tabs = [
+    'Breakdown',
+    'Budget',
+    'Characters',
+    'Locations',
+    'Bids',
+    'Agreements'
+  ];
+
+  actions = [
+    { label: 'CLOSE ALL', action: 'closeAll' },
+    { label: 'ADD SEQUENCE', action: 'addSequence' },
+    { label: 'GENERATE SHOTS', action: 'generateShots' },
+    { label: 'GENERATE VFX ASSUMPTIONS', action: 'generateAssumptions' },
+    { label: 'GENERATE COST ESTIMATES', action: 'generateEstimates' }
+  ];
+
+  constructor(private productionService: ProductionService) {
+    this.production = {} as Production;
+  }
+
+  ngOnInit(): void {
+    this.productionService.getProduction().subscribe(production => {
+      this.production = production;
+    });
+  }
+
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
+
+  performAction(action: string): void {
+    switch(action) {
+      case 'closeAll':
+        this.closeAllItems();
+        break;
+      case 'addSequence':
+        // TODO: Implement add sequence
+        break;
+      case 'generateShots':
+        // TODO: Implement generate shots
+        break;
+      case 'generateAssumptions':
+        // TODO: Implement generate assumptions
+        break;
+      case 'generateEstimates':
+        // TODO: Implement generate estimates
+        break;
+    }
+  }
+
+  closeAllItems(): void {
+    this.production.sequences.forEach(seq => {
+      seq.isCollapsed = true;
+      seq.scenes.forEach(scene => {
+        scene.isCollapsed = true;
+        scene.actionBeats.forEach(beat => {
+          beat.isCollapsed = true;
+        });
+      });
+    });
+  }
+
+  onSearch(): void {
+    // TODO: Implement search functionality
+    console.log('Searching for:', this.searchTerm);
+  }
+}
